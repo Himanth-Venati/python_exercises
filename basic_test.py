85206c43857466587423f3e5c3eb5ba2a6b63e1e
VenatiHimanth <himanth.venati9@gmail.com>
2022-01-21 19:07:34 +0100

    Update basic_test.py

--- basic_test.py
+++ basic_test.py
@@ -3,15 +3,20 @@
 # 2. School if the number is divisible by 5
 # 3. Strive School if the number is divisible by 3 and 5
 # 4. The same number otherwise (if the number is not divisible neither by 3 and neither by 5)
-def print_results(num):
-    if num % 3 == 0 and num % 5 ==0:
-        print('strive school')
-    elif num % 3 == 0:
-        print('strive')
-    elif num % 5 == 0:
-        print('school')
+import numpy as np
+x=np.array([5,3,15])
+def print_results(x):
+    for num in x:
+
+        if num % 3 == 0 and num % 5 ==0:
+            print('strive school')
+        elif num % 3 == 0:
+            print('strive')
+        elif num % 5 == 0:
+            print('school')
     
-    else:
-        print(num)
-    return 
-print_results(15)
+        else:
+            print(num)
+    return
+print_results(x)    
+
