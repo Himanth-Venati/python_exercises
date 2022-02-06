85981817c4d7afa563f90be761b19712da487301
VenatiHimanth <himanth.venati9@gmail.com>
2022-02-06 21:57:20 +0100

    Update test4.py

--- test4.py
+++ test4.py
@@ -18,38 +18,44 @@
 # A boolean indicating it the cubes intersect
 # and the volume of the shared space, if the cubes do not intersect then this should be 0
 
-cube1=5
-cube2=2
-Dcube1=[10,10,0]
-Dcube2=[8,9,0]
-def inter(cube1,cube2,x1,y1,z1,x2,y2,z2):
-    X1min=x1-(cube1/2) # for cube1 x-minimum
-    Y1min=y1-(cube1/2) #for cube1 y-minimum
-    Z1min=z1-(cube1/2) #for cube1 z-minimum
-    X2max=x2+(cube2/2) # for cube2 x-maximum
-    Y2min=y2-(cube2/2) #for cube2 y-minimum
-    Y2max=y2+(cube2/2) # for cube2 y-maximum
-    Z2max=z2+(cube2/2) # for cube2 z-maximum
-    if X1min>X2max:
-        return 0
-    if Y1min>Y2max:
-        return 0
-    if Z1min>Z2max:
-        return 0   
-    a=[]
-    if X2max>X1min:
-        a.append(X2max-X1min)
+class Intersec:
 
-    if Y2max>Y2min:
-        a.append(Y2max-Y2min)    
-    if z1==0 or z2==0:
-        a.append(float(cube2))
-    elif Z2max>Z1min:
-        a.append(Z2max-Z1min)
-    if a!=0:
-        intersection=['True',a[0]*a[1]*a[2]] 
+    def __init__(self,cube1,cube2,x1,y1,z1,x2,y2,z2):
+        self.cube1=cube1
+        self.cube2=cube2
+        self.x1=x1
+        self.y1=y1
+        self.z1=z1
+        self.x2=x2
+        self.y2=y2
+        self.z2=z2
 
-    else:
-        return False   
-    return intersection          
-inter(5,2,10,10,0,8,9,0)  
+
+    def inter(self):
+        X1min=self.x1-(self.cube1/2) #for cube1 x-minimum
+        Y1min=self.y1-(self.cube1/2) #for cube1 y-minimum
+        Z1min=self.z1-(self.cube1/2) #for cube1 z-minimum
+        X2max=self.x2+(self.cube2/2) #for cube2 x-maximum
+        Y2min=self.y2-(self.cube2/2) #for cube2 y-minimum
+        Y2max=self.y2+(self.cube2/2) #for cube2 y-maximum
+        Z2max=self.z2+(self.cube2/2) #for cube2 z-maximum
+        if X1min>X2max and Y1min>Y2max and Z1min>Z2max:
+            return False
+        a=[]
+        if X2max>X1min:
+            a.append(X2max-X1min)
+
+        if Y2max>Y2min:
+            a.append(Y2max-Y2min)    
+        if self.z1==0 or self.z2==0:
+            a.append(float(self.cube2))
+        elif Z2max>Z1min:
+            a.append(Z2max-Z1min) 
+        intersection=1
+        for i in a:
+            intersection*=i     
+        return True, intersection 
+
+inters=Intersec(5,2,10,10,0,8,9,0)   
+inters.inter()           
+ 
